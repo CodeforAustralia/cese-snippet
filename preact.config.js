67f8f4f0b2dd9bf5b3056d85ac84bde09814282f
Elise Chant <elise.chant@gmail.com>
2018-02-14 13:40:11 +1100

    bump preact cli; add webpacl overlay in dev

--- preact.config.js
+++ preact.config.js
@@ -3,6 +3,14 @@ const path = require('path');
 const appSrcPath = path.resolve(process.cwd(), 'src');
 
 module.exports = (config, env, helpers) => {
+
+  // resolve module paths
   const newResolveModules = [...config.resolve.modules, appSrcPath];
   config.resolve.modules = newResolveModules;
+
+  if (env === 'development') {
+    // show fullscreen error overlay
+    config.devServer.overlay = true;
+  }
+
 };
