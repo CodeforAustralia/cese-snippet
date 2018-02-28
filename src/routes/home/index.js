224304d422d2dc6f27ecc561b73537d7b5b6f4dd
Elise Chant <elise.chant@gmail.com>
2018-02-28 14:27:11 +1100

    add favicons; add simple meta;

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -6,7 +6,7 @@ export default class Home extends Component {
 		return (
 			<div class={style.home}>
 				<h1>Snippet</h1>
-        <h2>Programs happening in schools now.</h2>
+        <h2>Programs happening in schools now</h2>
         <p>For school staff to view or publish news about program applications in NSW schools.</p>
 			</div>
 		);
