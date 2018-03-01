09b5661bee30150842086e071aec9d366ae6a753
Elise Chant <elise.chant@gmail.com>
2018-03-02 09:03:48 +1100

    login look and feel

---
+++ src/layouts/login/index.js
@@ -0,0 +1,16 @@
+import { h } from 'preact';
+
+import style from './style.scss';
+
+
+const LoginLayout = ({children}) => (
+  <div className={`container-fluid ${style.container}`}>
+    <div className={`row ${style.centeredLayout}`}>
+      <div className="col-sm-9 col-md-3">
+        {children}
+      </div>
+    </div>
+  </div>
+);
+
+export default LoginLayout;
--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -1,8 +1,9 @@
 import { h, Component } from 'preact';
-import { route } from 'preact-router';
+import { route, Link } from 'preact-router';
 
-import Layout from 'layouts/account';
+import Layout from 'layouts/login';
 import style from './style.scss';
+import Logo from './logo.png';
 
 
 export default class Login extends Component {
@@ -13,13 +14,21 @@ export default class Login extends Component {
   render() {
     return (
       <Layout>
-        <div className={style.layoutCentered}>
+        <div>
+          <img className={`mb-4 ${style.logo}`} src={Logo} alt="" width="100" height="100" />
+          <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+          <p>If you're not sure what Snippet is, please read about it <Link href="/">here</Link>.</p>
+
           <form className={style.formSignin} onSubmit={this.onSubmit.bind(this)}>
-            <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-            <label for="inputEmail" className="sr-only">Email address</label>
-            <input type="email" id="inputEmail" className="form-control" placeholder="Email address" autofocus />
-            <label for="inputPassword" className="sr-only">Password</label>
-            <input type="password" id="inputPassword" className="form-control" placeholder="Password" />
+            <div className="form-group">
+              <label for="inputEmail" className="sr-only">Email address</label>
+              <input type="email" id="inputEmail" className="form-control" placeholder="Email address" autofocus />
+            </div>
+            <div className="form-group">
+              <label for="inputPassword" className="sr-only">Password</label>
+              <input type="password" id="inputPassword" className="form-control" placeholder="Password" />
+            </div>
+
             <div className="checkbox mb-3">
               <label>
                 <input type="checkbox" value="remember-me" /> Remember me
