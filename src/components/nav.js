868be1325afb04b28d3f4547616b1127222bfe54
Elise Chant <elise.chant@gmail.com>
2018-03-03 13:10:32 +1100

    tidy up look and feel (#33)

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 const Nav = () => (
   <ul>
-    <li><Link to="/" activeClassName="active">Home</Link></li>
-    <li><Link to="/login" activeClassName="active">Login</Link></li>
-    <li><Link to="/logout" activeClassName="active">Logout</Link></li>
+    {/*<li><Link to="/" activeClassName="active">Home</Link></li>*/}
+    {/*<li><Link to="/login" activeClassName="active">Login</Link></li>*/}
+    <li><Link to="/logout" activeclassname="active">Logout</Link></li>
     <li><Link to="/account/programs">Account Programs</Link></li>
-    <li><Link to="/account/programs/442" activeClassName="active">Account Program</Link></li>
-    <li><Link to="/account/programs/new" activeClassName="active">New Program</Link></li>
-    <li><Link to="/account/programs/785/edit" activeClassName="active">Account Program - Edit</Link></li>
+    <li><Link to="/account/programs/442" activeclassname="active">Account Program</Link></li>
+    <li><Link to="/account/new-program" activeclassname="active">New Program</Link></li>
+    <li><Link to="/account/programs/785/edit" activeclassname="active">Account Program - Edit</Link></li>
   </ul>
 );
 
