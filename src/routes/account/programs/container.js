88a0201ad5d45ee23306015e09fa98d10a5cacf1
Elise Chant <elise.chant@gmail.com>
2018-03-05 19:10:09 +1100

    remove backend; update firebase json;

--- src/routes/account/programs/container.js
+++ src/routes/account/programs/container.js
@@ -1,9 +1,8 @@
 import { connect } from 'react-redux';
-import { selectPrograms } from 'store/programs/ducks';
 
 export const mapStateToProps = (state) => {
   return {
-    programs: selectPrograms(state),
+    programs: state.programs,
   }
 };
 
