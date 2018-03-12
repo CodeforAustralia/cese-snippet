0c405d586d109f7c9967bd99ef5deb2a45df5953
Elise Chant <elise.chant@gmail.com>
2018-03-13 09:15:01 +1100

    test mock api (#43)
    * test fetch all
    
    * test fetch one
    
    * add tests for schools api
    
    * test applied program
    
    * test program
    
    * disable deploy for now

--- src/_api/index.js
+++ src/_api/index.js
@@ -1,22 +1,52 @@
+import queryString from 'query-string';
+import isEmpty from 'lodash/isEmpty';
+
 // This is a fake in-memory implementation of something
 // that would be implemented by calling a REST server.
 
-const fakeDatabase = {
+export const fakeDatabase = {
   "appliedPrograms": [
     {
       "id": 1,
       "programId": 1,
-      "name": "Our Program 1",
+      "name": "Mars Program 1",
       "schoolCode": 21312,
-      "facilitators": [8787],
+      "facilitators": [ 8787 ],
       "yearGroups": ["7", "8"],
       "year": "2018",
       "termStart": 3,
       "termEnd": 4,
       "cohortSize": 120,
-      "desiredOutcomes": "Student uplift in the history of the classic Lorem Ipsum passage and generate your own text using any number of characters, words, sentences or paragraphs.",
       "lastUpdatedBy": 8787,
+      "lastUpdated": "2018-02-04T23:56:38.363Z"
+    },
+    {
+      "id": 2,
+      "programId": 1,
+      "name": "Jupiter Program 1",
+      "schoolCode": 76862,
+      "facilitators": [ 23423 ],
+      "yearGroups": ["7", "8"],
+      "year": "2018",
+      "termStart": 3,
+      "termEnd": 4,
+      "cohortSize": 160,
+      "lastUpdatedBy": 23423,
       "lastUpdated": "2018-03-04T23:56:38.363Z"
+    },
+    {
+      "id": 3,
+      "programId": 1,
+      "name": "Jupiter Program 2",
+      "schoolCode": 76862,
+      "facilitators": [ 23423 ],
+      "yearGroups": ["7", "8"],
+      "year": "2017",
+      "termStart": 1,
+      "termEnd": 4,
+      "cohortSize": 30,
+      "lastUpdatedBy": 23423,
+      "lastUpdated": "2017-03-04T23:56:38.363Z"
     }
   ],
   "programs": [
@@ -33,73 +63,183 @@ const fakeDatabase = {
       "code": 21312,
       "name": "Mars High School",
       "type": "Secondary",
-      "programs": [1]
+    },
+    {
+      "code": 76862,
+      "name": "Jupiter High School",
+      "type": "Secondary",
     }
   ]
 };
 
-const delay = (ms = 1000) =>
+const delay = (ms = process.env.NODE_ENV === 'test' ? 0 : 1000) =>
   new Promise(resolve => setTimeout(resolve, ms));
 
 
 const mockApi = (path, payload) => {
-  // all schools
-  if (path.startsWith('/schools')) {
-    return delay().then(() => {
-      return { data: fakeDatabase.schools }
-    });
-  }
+
   // one school
   if (path.startsWith('/schools/')) {
     return delay().then(() => {
-      const s = fakeDatabase.schools.find(s => s.code === payload.code);
-      return { data: [s] }
+      const code = path.replace(/\/schools\/(.*)/i, '$1');
+      if (!code) {
+        throw new Error(`No code present: ${code}.`);
+      }
+      return {
+        data: {
+          school: fakeDatabase.schools.find(s => {
+            return Number(s.code) === Number(code);
+          }),
+        }
+      };
     });
   }
   // many schools (id is code)
   if (path.startsWith('/schools?')) {
-    throw new Error('not implemented');
-  }
-
-
-  // all appliedPrograms
-  if (path.startsWith('/appliedPrograms')) {
     return delay().then(() => {
-      return { data: fakeDatabase.appliedPrograms };
+      const searchParams = path.replace(/\/schools(.*)/i, '$1');
+      const params = queryString.parse(searchParams);
+      const { code } = params;
+
+      if (!code) {
+        throw new Error(`No code present: ${code}.`);
+      }
+      const resp = {
+        data: {
+          schools: null,
+        }
+      };
+      if (Array.isArray(code)) {
+        resp.data.schools = code.map(c => {
+          return fakeDatabase.schools.find(s => Number(s.code) === Number(c));
+        });
+      } else {
+        resp.data.schools = [fakeDatabase.schools.find(s => {
+          return Number(s.code) === Number(code);
+        })];
+      }
+      return resp;
     });
   }
+  // all schools
+  if (path.startsWith('/schools')) {
+    return delay().then(() => {
+      const resp = {
+        data: {
+          schools: fakeDatabase.schools,
+        }
+      };
+      return resp;
+    });
+  }
+
+
   // one appliedPrograms
   if (path.startsWith('/appliedPrograms/')) {
-    throw new Error('not implemented');
+    return delay().then(() => {
+      const id = path.replace(/\/appliedPrograms\/(.*)/i, '$1');
+      if (!id) {
+        throw new Error(`No id present: ${id}.`);
+      }
+      return {
+        data: {
+          appliedProgram: fakeDatabase.appliedPrograms.find(s => {
+            return Number(s.id) === Number(id);
+          }),
+        }
+      };
+    });
   }
   // many appliedPrograms
   // filtered appliedPrograms
   if (path.startsWith('/appliedPrograms?')){
     return delay().then(() => {
-      const { code, year } = payload;
-      const data = fakeDatabase.appliedPrograms.filter(program => {
-        return program.schoolCode === code;
-      }).filter(program => {
-        return program.year === year;
+      const searchParams = path.replace(/\/appliedPrograms(.*)/i, '$1');
+      const params = queryString.parse(searchParams);
+      const { id, schoolCode, year } = params;
+
+      if (isEmpty(params)) {
+        throw new Error(`No params present: ${JSON.stringify(params)}.`);
+      }
+
+      const resp = {
+        data: {
+          appliedPrograms: null,
+        }
+      };
+
+      if (typeof id !== 'undefined') {
+        if (Array.isArray(id)) {
+          resp.data.appliedPrograms = id.map(c => {
+            return fakeDatabase.appliedPrograms.find(p => Number(p.id) === Number(id));
+          });
+        } else {
+          resp.data.appliedPrograms = [fakeDatabase.appliedPrograms.find(p => {
+            return Number(p.id) === Number(id);
+          })];
+        }
+        return resp;
+      }
+
+      resp.data.appliedPrograms = fakeDatabase.appliedPrograms.filter(p => {
+        if (typeof schoolCode !== 'undefined') {
+          if (Number(p.schoolCode) !== Number(schoolCode)) {
+            return false;
+          }
+        }
+        if (typeof year !== 'undefined') {
+          if (p.year !== year) {
+            return false;
+          }
+        }
+        return true;
       });
-      return { data };
+      return resp;
     });
   }
-
-
-  // all programs
-  if (path.startsWith('/programs')) {
+  // all appliedPrograms
+  if (path.startsWith('/appliedPrograms')) {
     return delay().then(() => {
-      return { data: fakeDatabase.programs };
+      const resp = {
+        data: {
+          appliedPrograms: fakeDatabase.appliedPrograms,
+        }
+      };
+      return resp;
     });
   }
+
+
   // one programs
   if (path.startsWith('/programs/')) {
-    throw new Error('not implemented');
+    return delay().then(() => {
+      const id = path.replace(/\/programs\/(.*)/i, '$1');
+      if (!id) {
+        throw new Error(`No id present: ${id}.`);
+      }
+      return {
+        data: {
+          program: fakeDatabase.programs.find(p => {
+            return Number(p.id) === Number(id);
+          }),
+        }
+      };
+    });
   }
   // many programs
   if (path.startsWith('/programs?')) {
     throw new Error('not implemented');
+  }
+  // all programs
+  if (path.startsWith('/programs')) {
+    return delay().then(() => {
+      const resp = {
+        data: {
+          programs: fakeDatabase.programs,
+        }
+      };
+      return resp;
+    });
   }
 
 
---
+++ src/_api/mockApi.test.js
@@ -0,0 +1,138 @@
+/* global describe, it, expect */
+import mockApi, { fakeDatabase } from './index';
+
+describe('mockApi', () => {
+
+  // // one school
+  // '/schools/1'
+  // // many schools (id is code)
+  // '/schools?code=1&code=2'
+  // // all schools
+  // '/schools'
+
+  describe('Schools', () => {
+    it('should fetch all Schools', () => {
+      expect.assertions(2);
+      return mockApi('/schools').then((resp) => {
+        expect(resp.data).toBeDefined();
+        const { data: { schools } } = resp;
+        expect(schools.length).toEqual(fakeDatabase.schools.length);
+      });
+    });
+
+    it('should fetch a single School', () => {
+      expect.assertions(2);
+      return mockApi('/schools/76862').then((resp) => {
+        expect(resp.data).toBeDefined();
+        const { data: { school } } = resp;
+        expect(school.code).toBe(76862);
+      });
+    });
+
+    it('should fetch a single school by school code', () => {
+      expect.assertions(3);
+      return mockApi('/schools?code=76862').then((resp) => {
+        expect(resp.data).toBeDefined();
+        const { data: { schools } } = resp;
+        expect(schools.length).toBe(1);
+
+        const school = schools[0];
+        expect(school.code).toBe(76862);
+      });
+    });
+
+    it('should fetch many schools by school code', () => {
+      expect.assertions(4);
+      return mockApi('/schools?code=76862&code=21312').then((resp) => {
+        expect(resp.data).toBeDefined();
+        const { data: { schools } } = resp;
+        expect(schools.length).toBe(2);
+
+        expect(schools[0].code).toBe(76862);
+        expect(schools[1].code).toBe(21312);
+      });
+    });
+  });
+
+
+  // // one appliedPrograms
+  // '/appliedPrograms/1
+  // // many appliedPrograms
+  // // filtered appliedPrograms
+  // '/appliedPrograms?id=1&id=2
+  // // all appliedPrograms
+  // '/appliedPrograms'
+  describe('AppliedPrograms', () => {
+    it('should fetch all Applied Programs', () => {
+      expect.assertions(2);
+      return mockApi('/appliedPrograms').then((resp) => {
+        expect(resp.data).toBeDefined();
+        const { data: { appliedPrograms } } = resp;
+        expect(appliedPrograms.length).toEqual(fakeDatabase.appliedPrograms.length);
+      });
+    });
+
+    it('should fetch a single Applied Program', () => {
+      expect.assertions(2);
+      return mockApi('/appliedPrograms/2').then((resp) => {
+        expect(resp.data).toBeDefined();
+        const { data: { appliedProgram } } = resp;
+        expect(appliedProgram.id).toBe(2);
+      });
+    });
+
+    it('should fetch a single Applied Program by id', () => {
+      expect.assertions(3);
+      return mockApi('/appliedPrograms?id=2').then((resp) => {
+        expect(resp.data).toBeDefined();
+        const { data: { appliedPrograms } } = resp;
+        expect(appliedPrograms.length).toBe(1);
+
+        const appliedProgram = appliedPrograms[0];
+        expect(appliedProgram.id).toBe(2);
+      });
+    });
+
+    it('should fetch Applied Programs by filters', () => {
+      expect.assertions(4);
+      return mockApi('/appliedPrograms?schoolCode=76862&year=2018').then((resp) => {
+        expect(resp.data).toBeDefined();
+        const { data: { appliedPrograms } } = resp;
+        expect(Array.isArray(appliedPrograms)).toBe(true);
+
+        // assume array length
+        const appliedProgram = appliedPrograms[0];
+        expect(appliedProgram.schoolCode).toBe(76862);
+        expect(appliedProgram.year).toBe("2018");
+      });
+    });
+
+
+    // // one programs
+    // '/programs/1'
+    // // many programs
+    // '/programs?id=1&id=2'
+    // // all programs
+    // '/programs'
+    it('should fetch all Programs', () => {
+      expect.assertions(2);
+      return mockApi('/programs').then((resp) => {
+        expect(resp.data).toBeDefined();
+        const { data: { programs } } = resp;
+        expect(programs.length).toEqual(fakeDatabase.programs.length);
+      });
+    });
+
+    it('should fetch a single Program', () => {
+      expect.assertions(2);
+      return mockApi('/programs/1').then((resp) => {
+        expect(resp.data).toBeDefined();
+        const { data: { program } } = resp;
+        expect(program.id).toBe(1);
+      });
+    });
+
+  });
+
+});
+
