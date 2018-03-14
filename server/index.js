54c61165ffa5510f5fada485faf9e23dd9e397ce
Elise Chant <elise.chant@gmail.com>
2018-03-15 10:31:56 +1100

    port stuff; wildcard routing for Express; Dockerfile.dev for server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const express = require('express');
-const index = require('./routes/index');
 const jsonServer = require('json-server');
 
 const db = require('./db.json');
@@ -8,14 +7,17 @@ const db = require('./db.json');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
 
 // Optional except if you want to have JSON Server defaults
 // app.use('/api', jsonServer.defaults());
 app.use('/api', jsonServer.router(db));
 
+app.get('*', function (req, res) {
+	res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}`);
--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const { Router } = require('express');
-const router = Router();
-
-router.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'public/index.html'));
-});
-
-module.exports = router;
