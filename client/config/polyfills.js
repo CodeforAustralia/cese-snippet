e3e499a45589af4409d8f1038725ffdc848154ca
Elise Chant <elise.chant@gmail.com>
2018-03-28 14:35:44 +1100

    dont mock localstorage in prod

--- client/config/polyfills.js
+++ client/config/polyfills.js
@@ -20,24 +20,3 @@ Object.assign = require('object-assign');
 if (process.env.NODE_ENV === 'test') {
   require('raf').polyfill(global);
 }
-
-// mock local storage
-const localStorageMock = (function() {
-  let store = {};
-
-  return {
-    getItem: function(key) {
-      return store[key] || null;
-    },
-    setItem: function(key, value) {
-      store[key] = value.toString();
-    },
-    clear: function() {
-      store = {};
-    }
-  };
-})();
-
-Object.defineProperty(window, 'localStorage', {
-  value: localStorageMock
-});
