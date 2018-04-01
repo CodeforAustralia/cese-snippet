d9218e2b9d9c647baf44d808a183133b0fcb94c5
Elise Chant <elise.chant@gmail.com>
2018-04-01 16:56:17 +1000

    add real api service with db

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
-var functions = require('firebase-functions');
+const functions = require('firebase-functions');
 
-var app = require('./server');
+// const localServer = require('./local-server');
+const api = require('./api-service/src/app');
 
-exports.app = functions.https.onRequest(app);
+exports.app = functions.https.onRequest(api);
