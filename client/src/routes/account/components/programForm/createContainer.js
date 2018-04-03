1007cf22f81e6527adab625f7f00d30675cb773d
Elise Chant <elise.chant@gmail.com>
2018-04-04 07:21:23 +1000

    field year and terms

--- client/src/routes/account/components/programForm/createContainer.js
+++ client/src/routes/account/components/programForm/createContainer.js
@@ -2,12 +2,14 @@ import { connect } from 'react-redux';
 import { createProgram } from 'store/programs/actionCreators';
 import { selectSession } from "store/session/selectors";
 import { selectSchools } from "store/schools/selectors";
+import { getYear } from 'utils/formatDate';
 
 const mapStateToProps = (state, ownProps) => {
   const { location } = ownProps;
   const session = selectSession(state);
   const schools = selectSchools(state, session.schools);
   const initialFormState = location.state && location.state.initialFormState || {};
+  const year = location.state && location.state.year || getYear();
 
   const newInitialFormState = {
     ...initialFormState,
@@ -25,6 +27,15 @@ const mapStateToProps = (state, ownProps) => {
         return [];
       }
       return school.yearLevels.map((y) => ({ value: y, label: y }));
+    },
+    year,
+    getTermsOptions: () => {
+      return [
+        { value: '1', label: `T1 - ${year}` },
+        { value: '2', label: `T2 - ${year}` },
+        { value: '3', label: `T3 - ${year}` },
+        { value: '4', label: `T4 - ${year}` },
+      ]
     }
   }
 };
