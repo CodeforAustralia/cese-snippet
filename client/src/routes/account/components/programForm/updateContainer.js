ae3124ba26d78f45ef81e99abce18445f5f4e23e
Elise Chant <elise.chant@gmail.com>
2018-04-04 14:11:52 +1000

    clone createContainer to updateContainer

--- client/src/routes/account/components/programForm/updateContainer.js
+++ client/src/routes/account/components/programForm/updateContainer.js
@@ -1,7 +1,9 @@
 import { connect } from 'react-redux';
 import { updateProgram } from 'store/programs/actionCreators';
 import { selectSession } from "store/session/selectors";
-import { selectSchools } from "store/schools/selectors";
+import { selectSchool } from "store/schools/selectors";
+
+
 
 /*
 
@@ -13,20 +15,41 @@ import { selectSchools } from "store/schools/selectors";
 
 const mapStateToProps = (state, ownProps) => {
   const { location } = ownProps;
+  const { code, year } = location.state.initialFormState;
+
   const session = selectSession(state);
-  const schools = selectSchools(state, session.schools);
+  const school = selectSchool(state, code);
+
   const initialFormState = location.state && location.state.initialFormState || {};
+
+  const codeOptions =[{ value: code, label: school.name }];
 
   const newInitialFormState = {
     ...initialFormState,
     updatedBy: session.id,
   };
 
+  if (typeof newInitialFormState.code === 'undefined' && codeOptions.length === 1) {
+    newInitialFormState.code = codeOptions[0].value;
+  }
+
   return {
     isEdit: true,
     initialFormState: newInitialFormState,
 
-    codeOptions: schools.map(s => ({ value: s.code, label: s.name })),
+    codeOptions,
+    getYearLevelsOptions: (code) => {
+      return school.yearLevels.map((y) => ({ value: y, label: y }));
+    },
+    year,
+    getTermsOptions: () => {
+      return [
+        { value: '1', label: `T1 - ${year}` },
+        { value: '2', label: `T2 - ${year}` },
+        { value: '3', label: `T3 - ${year}` },
+        { value: '4', label: `T4 - ${year}` },
+      ]
+    }
   }
 };
 
