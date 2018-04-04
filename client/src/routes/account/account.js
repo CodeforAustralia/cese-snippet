b9319038e505d207a49666ebfb8a92c92204522f
Elise Chant <elise.chant@gmail.com>
2018-04-04 15:26:41 +1000

    remove redundant

--- client/src/routes/account/account.js
+++ client/src/routes/account/account.js
@@ -3,9 +3,6 @@ import {
   Route,
   Switch,
 } from "react-router-dom";
-import {
-  Container
-} from 'reactstrap';
 
 import AccountLayout from './layouts';
 import School from './school';
@@ -52,22 +49,18 @@ class Account extends React.Component {
 
     return (
       <AccountLayout>
-
         { isFetching !== false ?
           <p>Loading...</p> :
           !schools.length ?
             <p>No schools</p> :
             <p>Programs: {schools.map((school, idx) => <span key={idx}>{school.name}</span>)}</p>
         }
-
-        <Container>
-          <Switch location={isModal ? this.previousLocation : location}>
-            <Route path="/account/schools/:code" exact component={School} />
-            <Route path="/account/schools/:code/programs/:year" component={SchoolPrograms} />
-            <Route path="/account/create-program" component={SchoolCreateProgram} />
-            <Route path="/account/register" component={RegisterFlow} />
-          </Switch>
-        </Container>
+        <Switch location={isModal ? this.previousLocation : location}>
+          <Route path="/account/schools/:code" exact component={School} />
+          <Route path="/account/schools/:code/programs/:year" component={SchoolPrograms} />
+          <Route path="/account/create-program" component={SchoolCreateProgram} />
+          <Route path="/account/register" component={RegisterFlow} />
+        </Switch>
         {isModal ? <Route path="/account/create-program" component={CreateProgramModal} /> : null}
       </AccountLayout>
     )
