1a79e80ef48f799f18cd62536776dcdfc4a1ade4
Elise Chant <elise.chant@gmail.com>
2018-04-05 09:44:23 +1000

    update dummy nav

--- client/src/routes/account/components/accountNav/index.js
+++ client/src/routes/account/components/accountNav/index.js
@@ -19,11 +19,11 @@ const AccountNav = () => {
   return (
     <Navbar color="light" light expand="md">
       <ul>
-        <li><NavLink to="/account/schools/76862">Jupiter School</NavLink></li>
-        <li><NavLink to={getSchoolProgramsUrl("76862", "2018")}>Jupiter School Programs 2018</NavLink></li>
+        <li><NavLink to="/account/schools/76862">Croppa Creek Public School</NavLink></li>
+        <li><NavLink to={getSchoolProgramsUrl("3717", "2018")}>Croppa Creek Public School Programs 2018</NavLink></li>
         <li><NavLink to={getCreateProgramUrl()}>Create Program</NavLink></li>
-        <li><NavLink to={getCreateProgramModalUrl({code: "76862", year: "2018"})}>Open Create Program modal</NavLink></li>
-        <li><NavLink to={getCreateProgramModalUrl({code: "76862", year: "2018", id: "2"})}>Open Update Program modal</NavLink></li>
+        <li><NavLink to={getCreateProgramModalUrl({code: "3717", year: "2018"})}>Open Create Program modal</NavLink></li>
+        <li><NavLink to={getCreateProgramModalUrl({code: "3717", year: "2018", id: "1"})}>Open Update Program modal</NavLink></li>
       </ul>
 
       <Container>
