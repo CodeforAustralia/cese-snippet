6ce2e938f6ca389c3fabbfe06334a2bb7063b006
Elise Chant <elise.chant@gmail.com>
2018-04-10 07:20:34 +1000

    fix production visibility issues

--- functions/api-service/src/middleware/index.js
+++ functions/api-service/src/middleware/index.js
@@ -1,5 +1,19 @@
+const staffListData = require('./staff-list.json');
+const programFieldsData = require('./program-field-data.json');
+
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   // Add your custom middleware here. Remember, that
   // in Express the order matters
+
+	app.get('/static/program-fields.json', (req, res) => {
+		res.setHeader('Content-Type', 'application/json');
+		res.send(programFieldsData);
+		return res;
+	});
+	app.get('/static/staff-list.json', (req, res) => {
+		res.setHeader('Content-Type', 'application/json');
+		res.send(staffListData);
+		return res;
+	});
 };
