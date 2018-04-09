4d764e86c9bd655bf9a6d1f69a32f14cd0a54b92
Elise Chant <elise.chant@gmail.com>
2018-04-09 15:40:54 +1000

    add program templates data

---
+++ client/mock-server/parsers/programTemplates.js
@@ -0,0 +1,33 @@
+// node parsers/programTemplates.js | pbcopy
+
+const arrayify = require('./arrayify');
+const raw = require('./../raw/programTemplates.json');
+
+const makeJson = (data) => {
+  return JSON.stringify(data.map(d => {
+
+    return {
+      "category": d.programArea,
+      "subCategory": d.programCategory,
+      "aims": d.aims,
+      "description": d.description,
+      "descriptionFull": d.descriptionFull,
+      "website": d.website,
+      "participantGroups": arrayify(d.participantGroups),
+      "participantGroupsDescription": d.participantGroupsDescription,
+      "focusGroup": d.focusGroup,
+      "focusGroupOther": d.focusGroupOther,
+      "yearLevel": arrayify(d.yearLevel),
+      "deliveredByType": d.deliveredByType,
+      "externalProvider": d.externalProvider,
+      "tags": arrayify(d.tags),
+      "createdAt": d.createdAt,
+      "createdBy": String(d.createdBy),
+      "updatedBy": String(d.updatedBy),
+      "updatedAt": d.updatedAt,
+      "name": d.name,
+    }
+  }));
+};
+
+console.log(makeJson(raw));
