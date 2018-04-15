3e6d86e3aedec5be27b27afb6f56ffe62cf6eb7a
Elise Chant <elise.chant@gmail.com>
2018-04-16 08:57:12 +1000

    add banner

--- client/src/routes/home/index.js
+++ client/src/routes/home/index.js
@@ -29,8 +29,8 @@ const HomePage = () => (
           <Col sm={{size: 7}}>
             <div className={style.headline}>
               <h2>Programs happening in schools now</h2>
-              <p className="h4">For school staff to view or publish news about program applications in NSW Public Schools.</p>
-              <p className={style.headlineCta}><Button size="lg" color="primary" tag={RRLink} to="/login">Start Demo</Button></p>
+              <p className="h4">For school staff to discover and publish practical knowledge, insights and resources about programs in NSW Public Schools.</p>
+              <p className={style.headlineCta}><Button size="lg" color="pink" tag={RRLink} to="/login">Start Demo</Button></p>
             </div>
           </Col>
         </Row>
@@ -39,35 +39,52 @@ const HomePage = () => (
           <Col sm={{size: 3}}>
             <div className={style.featureBlock}>
               <p>Icon</p>
-              <h3 className="h4">Feature one</h3>
-              <p>Clearly describe what tangible feature one in this product does.</p>
+              <h3 className="h4">School level view</h3>
+              <p>View and manage all of your school's active programs, update them and create new ones. View programs historically from your school too.</p>
+            </div>
+          </Col>
+          {/*<Col sm={{size: 3}}>*/}
+            {/*<div className={style.featureBlock}>*/}
+              {/*<Badge color="warning" pill className={style.comingSoonBadge}>Coming soon</Badge>*/}
+              {/*<p>Icon</p>*/}
+              {/*<h3 className="h4">Attach documents</h3>*/}
+            {/*</div>*/}
+          {/*</Col>*/}
+          <Col sm={{size: 3}}>
+            <div className={style.featureBlock}>
+              <Badge color="info" pill className={style.comingSoonBadge}>Coming soon</Badge>
+              <p>Icon</p>
+              <h3 className="h4">Fast publishing</h3>
+              <p>Publish and share resources like work sheets, documents and photographs about active school programs in real time from your smartphone. Build the rich story of each program over time.</p>
             </div>
           </Col>
           <Col sm={{size: 3}}>
             <div className={style.featureBlock}>
+              <Badge color="info" pill className={style.comingSoonBadge}>Coming soon</Badge>
               <p>Icon</p>
-              <h3 className="h4">Feature two</h3>
-              <p>Clearly describe what tangible feature two in this product does.</p>
+              <h3 className="h4">Intelligent Search</h3>
+              <p>Research tools allow you to earch for programs occurring in other schools using local and specific search criteria to find programs used in others schools in conditions alike your own.</p>
             </div>
           </Col>
           <Col sm={{size: 3}}>
             <div className={style.featureBlock}>
+              <Badge color="info" pill className={style.comingSoonBadge}>Coming soon</Badge>
               <p>Icon</p>
-              <h3 className="h4">Feature three <Badge color="light" pill className={style.comingSoonBadge}>Coming soon</Badge></h3>
-              <p>Clearly describe what tangible feature three in this product does.</p>
-            </div>
-          </Col>
-          <Col sm={{size: 3}}>
-            <div className={style.featureBlock}>
-              <p>Icon</p>
-              <h3 className="h4">Feature four <Badge color="light" pill className={style.comingSoonBadge}>Coming soon</Badge></h3>
-              <p>Clearly describe what tangible feature four in this product does.</p>
+              <h3 className="h4">Fresh content</h3>
+              <p>Over time we'll learn about you and your school and we'll deliver relevant content to you about programs happening at your school as well as those happening in all NSW Public Schools.</p>
             </div>
           </Col>
         </Row>
       </Container>
+
+
     </div>
 
+    <div className={style.alertBanner}>
+      <Container>
+        <p>Staff from over 1000 schools have already signed up.</p>
+      </Container>
+    </div>
 
     <div className={style.lowerLayoutBand}>
       <Container>
@@ -139,7 +156,7 @@ const HomePage = () => (
           <Col>
             <Form inline>
               <Label className="mr-2">I'm interested and I want to get started!</Label>
-              <Button size="lg" color="primary" tag={RRLink} to="/login" className="ml-2">Start Demo</Button>
+              <Button size="lg" color="pink" tag={RRLink} to="/login" className="ml-2">Start Demo</Button>
             </Form>
           </Col>
         </Row>
@@ -150,7 +167,7 @@ const HomePage = () => (
     <div className={style.footerBand}>
       <Container>
         <Col size={{sm: 2}}>
-          <img src={DetLogo} width={130} height={47} />
+          <img src={DetLogo} width={130} height={47} alt="NSW Department of Education logo" />
         </Col>
       </Container>
     </div>
