0d869c537cf2ed8ba735759c01c8b2120af522f1
Elise Chant <elise.chant@gmail.com>
2018-04-17 12:04:05 +1000

    improve login layout (#127)

---
+++ client/src/routes/home/layout.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  Navbar,
+  NavbarBrand,
+  Badge,
+} from "reactstrap";
+import cx from 'classnames';
+
+import style from './layout.scss';
+
+const Layout = ({containerClassName, children}) => (
+  <div className={cx(containerClassName, style.layout)}>
+
+    <Navbar fixed color="transparent" className={style.navbar}>
+      <NavbarBrand to="/" className={style.navbarBrand}>Snippet<Badge color="light" pill className={style.alphaBadge}>ALPHA</Badge></NavbarBrand>
+    </Navbar>
+
+    {children}
+
+  </div>
+);
+
+export default Layout;
