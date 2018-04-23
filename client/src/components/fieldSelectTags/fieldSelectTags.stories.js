61417e85c18f5c420b59c1a985c19c38f622b70d
Elise Chant <elise.chant@gmail.com>
2018-04-24 08:38:37 +1000

    fix storybook

--- client/src/components/fieldSelectTags/fieldSelectTags.stories.js
+++ client/src/components/fieldSelectTags/fieldSelectTags.stories.js
@@ -2,13 +2,31 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import FieldSelectTags from './index';
-import STAFF_DATA from 'static/staff.json';
+
+
+const staffOptions = [
+  {
+    "value": "37171",
+    "label": "Malorie.Mahn@test.nsw.edu.au"
+  },
+  {
+    "value": "37172",
+    "label": "Kenda.Kist@test.nsw.edu.au"
+  },
+  {
+    "value": "41181",
+    "label": "Leontine.Leija@test.nsw.edu.au"
+  },
+  {
+    "value": "41182",
+    "label": "Aurelio.Aikins@test.nsw.edu.au"
+  },
+];
 
 storiesOf('Field - select tags', module)
   .add('default', () => {
-    const options = STAFF_DATA.staff.map((staff) => ({ value: staff.id, label: staff.email }));
     return (
-      <FieldSelectTags name="staff" options={options} />
+      <FieldSelectTags name="staff" options={staffOptions} />
     )
   })
 ;
