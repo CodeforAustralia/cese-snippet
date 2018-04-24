61ea1a549562501978aba1f46ea68e3e07a87165
Elise Chant <elise.chant@gmail.com>
2018-04-24 12:44:24 +1000

    test cases for field select (#140)

--- client/src/components/fieldSelect/fieldSelect.stories.js
+++ client/src/components/fieldSelect/fieldSelect.stories.js
@@ -1,24 +1,133 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
+import {
+  Formik,
+  Form,
+} from 'formik';
+
 import FieldSelect from './index';
 
-storiesOf('Field - category', module)
-  .add('default', () => {
-    const values = {};
-    const errors = {};
-    const touched = {};
-    const setFieldValue = () => {};
-    const setFieldTouched = () => {};
+
+const options = [
+  { value: '1', label: 'One' },
+  { value: '2', label: 'Two' },
+  { value: '3', label: 'Three' },
+];
+
+storiesOf('Field select', module)
+
+  .add('should select a single value', () => {
     return (
-      <FieldSelect
-        name="category"
-        value={values.category}
-        onChange={setFieldValue}
-        onBlur={setFieldTouched}
-        error={errors.category}
-        touched={touched.category}
-      />
+      <Formik
+        render={({values, setFieldValue, setFieldTouched}) => {
+          return (
+            <Form>
+              <FieldSelect name="prop1"
+                           options={options}
+                           value={values.prop1}
+                           onChange={setFieldValue}
+                           onBlur={setFieldTouched}
+                           clearable={false}
+                           placeholder="Select..."
+              />
+              <code>
+                Form state: {JSON.stringify(values)}
+              </code>
+            </Form>
+          )
+        }} />
+    )
+  })
+
+  .add('single value should be clearable', () => {
+    return (
+      <Formik
+        render={({values, setFieldValue, setFieldTouched}) => {
+          return (
+            <Form>
+              <FieldSelect name="prop1"
+                           options={options}
+                           value={values.prop1}
+                           onChange={setFieldValue}
+                           onBlur={setFieldTouched}
+              />
+              <code>
+                Form state: {JSON.stringify(values)}
+              </code>
+            </Form>
+          )
+        }} />
+    )
+  })
+
+  .add('should preload data if provided', () => {
+    return (
+      <Formik
+        initialValues={{prop1: '3'}}
+        render={({values, setFieldValue, setFieldTouched}) => {
+          return (
+            <Form>
+              <FieldSelect name="prop1"
+                           options={options}
+                           value={values.prop1}
+                           onChange={setFieldValue}
+                           onBlur={setFieldTouched}
+              />
+              <code>
+                Form state: {JSON.stringify(values)}
+              </code>
+            </Form>
+          )
+        }} />
+    )
+  })
+
+  .add('should update value imperatively', () => {
+    return (
+      <Formik
+        render={({values, setFieldValue, setFieldTouched}) => {
+          return (
+            <div>
+              <button type="button" onClick={() => setFieldValue('prop1', '2')}>Set prop1 to "2"</button>
+              <Form>
+                <FieldSelect name="prop1"
+                             options={options}
+                             value={values.prop1}
+                             onChange={setFieldValue}
+                             onBlur={setFieldTouched}
+                />
+                <code>
+                  Form state: {JSON.stringify(values)}
+                </code>
+              </Form>
+            </div>
+          )
+        }} />
+    )
+  })
+
+  .add('should update value imperatively - bulk', () => {
+    return (
+      <Formik
+        render={({values, setFieldValue, setFieldTouched, setValues}) => {
+          return (
+            <div>
+              <button type="button" onClick={() => setValues({prop1: '2'})}>Set prop1 to "2"</button>
+              <Form>
+                <FieldSelect name="prop1"
+                             options={options}
+                             value={values.prop1}
+                             onChange={setFieldValue}
+                             onBlur={setFieldTouched}
+                />
+                <code>
+                  Form state: {JSON.stringify(values)}
+                </code>
+              </Form>
+            </div>
+          )
+        }} />
     )
   })
 ;
