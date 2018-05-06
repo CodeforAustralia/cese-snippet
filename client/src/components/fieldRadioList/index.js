ff183289d24b040dc39448b1233248f79b7815b5
Elise Chant <elise.chant@gmail.com>
2018-05-06 14:48:00 +1000

    checkbox list styling; improve radio list styling; (#175)

--- client/src/components/fieldRadioList/index.js
+++ client/src/components/fieldRadioList/index.js
@@ -1,43 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   Label,
   Input,
+  FormFeedback,
 } from 'reactstrap';
 import {
   FieldArray,
 } from 'formik';
 
-const FieldRadioBtnList = ({ options, name, value }) => {
+const FieldRadioBtnList = ({ options, name, value, error = null }) => {
   return (
     <div>
-      <FieldArray name={name}
-                  render={({form}) => (
-                    options.map((o, idx) => {
-                      const oName = `${name}.${o.value}`;
-                      const isChecked = value === o.value;
-                      return (
-                        <div key={idx} className="custom-control custom-radio">
-                          <Input type="radio"
-                                 id={oName}
-                                 name={name}
-                                 checked={isChecked}
-                                 onChange={() => form.setFieldValue(name, o.value)}
-                                 onBlur={form.setFieldTouched}
-                                 className={`
-                                  custom-control-input
-                                  ${form.errors[name] && 'is-invalid'}
-                                 `}
-                          />
-                          <Label htmlFor={oName} className='custom-control-label'>
-                            {o.label}
-                          </Label>
-                        </div>
-                      )
-                    })
-                  )}
+      <FieldArray
+        name={name}
+        render={({form}) => (
+          options.map((o, idx) => {
+            const oName = `${name}.${o.value}`;
+            const isChecked = value === o.value;
+            return (
+              <div key={idx} className="custom-control custom-radio">
+                <Input type="radio"
+                       id={oName}
+                       name={name}
+                       checked={isChecked}
+                       onChange={() => form.setFieldValue(name, o.value)}
+                       onBlur={form.setFieldTouched}
+                       className={`
+                        custom-control-input
+                        ${error && 'is-invalid'}
+                       `}
+                />
+                <Label htmlFor={oName} className='custom-control-label'>{o.label}</Label>
+              </div>
+            )
+          })
+        )}
       />
+      {error && <FormFeedback style={{display:'block'}}>{error}</FormFeedback>}
     </div>
   );
+};
+
+FieldRadioBtnList.propTypes = {
+  name: PropTypes.string.isRequired,
+  options: PropTypes.array,
 };
 
 export default FieldRadioBtnList;
