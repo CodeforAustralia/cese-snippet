32b1f49222dff54a4dfd51e5c861d78c95476e71
Suman Kathimanda <sumoz.work@gmail.com>
2018-05-08 16:02:54 +1000

    unstyled checkbox

--- client/src/routes/account/components/selectAllYears/index.js
+++ client/src/routes/account/components/selectAllYears/index.js
@@ -5,11 +5,18 @@ import {
 } from 'reactstrap';
 import style from './style.scss'
 
+// const checkboxStyle = {
+//     marginTop: '0.3rem',
+//     boxSizing: 'border-box',
+//     padding: '0'
+// }
+
+
 
 const SelectAllYears = (props, error=null, inline=false) => {
 
     return (
-        <div>
+        <div className={`custom-control custom-checkbox ${inline && style.isInline}`}>
             <Input type="checkbox"
                 onClick={(event) => {
                 let selectAll = [];
@@ -18,9 +25,8 @@ const SelectAllYears = (props, error=null, inline=false) => {
                       ? props.allYears("yearLevels", selectAll)
                       : props.allYears("yearLevels", [])
                 }} />
-            <Label>Select all years</Label>
+            <Label>Select all levels</Label>
         </div>
-
     )
 }
 
