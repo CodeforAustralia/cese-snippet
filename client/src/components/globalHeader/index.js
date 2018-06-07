e5708bcea580da381e0deb0d4e85b94319f9bd8e
Elise Chant <elise.chant@gmail.com>
2018-06-07 15:12:53 +1000

    remove redundant

--- client/src/components/globalHeader/index.js
+++ client/src/components/globalHeader/index.js
@@ -2,12 +2,12 @@ import { withRouter } from 'react-router-dom';
 
 import withAuth from 'components/auth/withAuth';
 
-import Container from './container';
+import AuthContainer from './authContainer';
 import Header from './globalHeaderAuth';
 import HeaderAuth from './globalHeaderAuth';
 
 const GlobalHeader = Header;
-const GlobalHeaderAuth = withRouter(withAuth(Container(HeaderAuth)));
+const GlobalHeaderAuth = withRouter(withAuth(AuthContainer(HeaderAuth)));
 
 export {
   GlobalHeader,
