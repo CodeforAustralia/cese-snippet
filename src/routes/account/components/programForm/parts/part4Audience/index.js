bb62bda3ed959d517915191035b07fd7496766eb
Elise Chant <elise.chant@gmail.com>
2018-06-29 15:16:47 +1000

    conditional for audience scope

--- src/routes/account/components/programForm/parts/part4Audience/index.js
+++ src/routes/account/components/programForm/parts/part4Audience/index.js
@@ -35,37 +35,41 @@ const Part4Audience = ({
           </Col>
         </FormGroup>
 
-        <FormGroup row>
-          <Col md={8}>
-            <Label className="mb-1">Year Levels</Label>
-            <FormText color="muted" className="mt-0 mb-3">
-              Which year levels are participating in this program?
-            </FormText>
-            <FieldCheckboxBtnList name="yearLevels"
-                               value={values.yearLevels}
-                               options={optionsYearLevels}
-                               error={errors.yearLevels}
-                               inline={true}
-            />
-          </Col>
-        </FormGroup>
+        {values.audienceScope && values.audienceScope !== 'Whole School' &&
+          <FormGroup row>
+            <Col md={8}>
+              <Label className="mb-1">Year Levels</Label>
+              <FormText color="muted" className="mt-0 mb-3">
+                Which year levels are participating in this program?
+              </FormText>
+              <FieldCheckboxBtnList name="yearLevels"
+                                 value={values.yearLevels}
+                                 options={optionsYearLevels}
+                                 error={errors.yearLevels}
+                                 inline={true}
+              />
+            </Col>
+          </FormGroup>
+        }
 
-        <FormGroup row>
-          <Col md={8}>
-            <Label htmlFor="cohortSize" className="mb-1">Number of Participants</Label>
-            <FormText color="muted" className="mt-0 mb-3">
-              How many people participated in this program?
-            </FormText>
-            <FieldRadioBtnList name="cohortSize"
-                               options={optionsCohortSize}
-                               error={errors.cohortSize}
-                               value={values.cohortSize}
-                               onChange={setFieldValue}
-                               inline={true}
-                               spaced={true}
-            />
-          </Col>
-        </FormGroup>
+        {values.audienceScope && (values.audienceScope === 'Mandatory Selected Groups' || values.audienceScope === 'Voluntary Participation') &&
+          <FormGroup row>
+            <Col md={8}>
+              <Label htmlFor="cohortSize" className="mb-1">Number of Participants</Label>
+              <FormText color="muted" className="mt-0 mb-3">
+                How many people participated in this program?
+              </FormText>
+              <FieldRadioBtnList name="cohortSize"
+                                 options={optionsCohortSize}
+                                 error={errors.cohortSize}
+                                 value={values.cohortSize}
+                                 onChange={setFieldValue}
+                                 inline={true}
+                                 spaced={true}
+              />
+            </Col>
+          </FormGroup>
+        }
       </div>
     </IndexedPartLayout>
   )
