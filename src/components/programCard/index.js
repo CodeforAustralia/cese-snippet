d0ec174ffa85ae1b966849c169a83ebbca958f77
Elise Chant <elise.chant@gmail.com>
2018-07-25 16:10:40 +1000

    add user menu items and new program cards that include snippets (#207)
    * add user menu items
    
    * new program cards include snippets
    
    * finish program cards
    
    * +1

---
+++ src/components/programCard/index.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import {
+  Card,
+  CardTitle,
+  CardText,
+  Media,
+  Button,
+} from 'reactstrap';
+import cx from 'classnames';
+import { Link as RRLink } from 'react-router-dom';
+import TruncatedText from 'components/truncatedText';
+
+import { getHumanisedMetaDescription } from 'store/programs/helpers';
+import style from './style.scss';
+
+const ProgramCard = ({ program, snippets = null }) => {
+
+  const hasEdited = !!program.updatedBy;
+  const metaText = getHumanisedMetaDescription(program);
+
+  const hasNotEnteredDetails = !program.description && !metaText;
+
+  return (
+    <Card>
+      <CardTitle className={style.title}>{program.name}</CardTitle>
+
+      {hasNotEnteredDetails && <CardText className={cx(style.description, 'text-muted')}>No details entered for "{program.name}". Help record the initiative <RRLink to="/">add information</RRLink>.</CardText>}
+
+      {program.description && <CardText className={style.description}>
+          <TruncatedText text={program.description} length={160} />
+        </CardText>}
+
+      {metaText && <p className={style.bannerMeta}>{metaText}</p>}
+
+      <div className={style.actions}>
+        <p className={style.actionTextLhs}>
+          <RRLink to="/">{hasEdited ? 'Edit details >' : 'Add details >'}</RRLink>
+        </p>
+        {!hasNotEnteredDetails && <p className={style.actionTextRhs}>
+          <RRLink to="/">View {`>`}</RRLink>
+        </p>}
+      </div>
+
+      <Card body className={style.snippetCard}>
+        <CardTitle className={style.snippetCardTitle}>Snippets
+          {snippets && <Button color="primary" outline size="xs" className={style.snippetAddButton}>Share another</Button>}
+        </CardTitle>
+
+        {snippets ?
+
+          <div>
+            <div className={cx(
+              style.snippetList,
+              snippets.length && snippets.length > 2 ? style.showGradient : '',
+            )}>
+
+              {snippets.map((snippet, key) => (
+                <Media className={style.snippet} key={key}>
+                  <Media body className={style.snippetBody}>
+                    <Media heading className={style.snippetDescription}>
+                      <TruncatedText text={snippet.description} length={160} />
+                    </Media>
+                  </Media>
+                  {snippet.type === 'photo' &&
+                    <Media right middle className={style.snippetImageRight}>
+                      <img src={snippet.attachment.url} alt="Snippet thumbnail" className="media-object" width={120} height={120} />
+                    </Media>
+                  }
+                </Media>
+              ))}
+
+            </div>
+
+            <div className={style.snippetListFooter}>
+              <span className={style.snippetListMeta}>{snippets.length && snippets.length === 1 ? `${snippets.length} Snippet` : `${snippets.length} Snippets`} </span>
+              <RRLink to="/" className={style.snippetListMore}>More Snippets ></RRLink>
+            </div>
+          </div> :
+
+          <p className="text-muted">No Snippets yet for "{program.name}". Get started recording moments <RRLink to="/">add the first Snippet</RRLink>.</p>
+        }
+
+      </Card>
+
+    </Card>
+  )
+};
+
+export default ProgramCard;
---
+++ src/components/programCard/programCard.stories.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { MemoryRouter } from 'react-router';
+
+
+import ProgramCard from './index';
+
+
+const fixture_programLightInfo = {
+  "id": "3",
+  "name": "Coding Club",
+  "schoolCode": "4548",
+  "year": "2018",
+  "createdBy": "1",
+  "updatedDate": 1530238113250,
+  "createdDate": 1530238113250
+};
+
+const fixture_programFullInfo = {
+  "id": "1",
+  "schoolCode": "4548",
+  "category": "Curriculum Engagement",
+  "subCategory": "Literacy",
+  "aims": "L3 is a whole-class intervention that aims to reduce the\nrisk of students not achieving expected literacy levels by\nthe end of their first year of schooling.",
+  "description": "L3 is a research-based Kindergarten classroom\nintervention, targeting text reading and writing. It provides\nrich literacy experiences through systematic and explicit\nteaching.",
+  "descriptionFull": null,
+  "website": "https://education.nsw.gov.au/teaching-and-learning/curriculum/literacy-and-numeracy/literacy/Language,-Learning-and-Literacy",
+  "participantGroups": [
+    "Students"
+  ],
+  "participantGroupsDescription": null,
+  "focusGroup": null,
+  "focusGroupOther": null,
+  "yearLevels": [
+    "K",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6"
+  ],
+  "cohortSize": 20,
+  "deliveredByType": "School Staff",
+  "externalProvider": null,
+  "staff": [
+    "1"
+  ],
+  "year": 2018,
+  "terms": [
+    1
+  ],
+  "tags": [
+    "reading",
+    "writing",
+    "vocabulary"
+  ],
+  "createdDate": 1530238113250,
+  "createdBy": "1",
+  "updatedBy": "1",
+  "updatedDate": 1530238113250,
+  "name": "Language, Learning and Literacy (L3)"
+};
+
+const fixture_snippet1 = {
+  "id": "1",
+  "type": "photo",
+  "description": "Ubi est bi-color era?",
+  "programId": "1",
+  "attachment": {
+    "format": "jpeg",
+    "width": 600,
+    "height": 400,
+    "filename": "image01.jpg",
+    "url": "https://picsum.photos/600/400"
+  },
+  "createdBy": "1",
+  "createdDate": 1530238113250
+};
+const fixture_snippet2 = {
+  "id": "2",
+  "type": "photo",
+  "description": "Est varius indictio, cesaris?",
+  "programId": "1",
+  "attachment": {
+    "format": "jpeg",
+    "width": 600,
+    "height": 400,
+    "filename": "image02.jpg",
+    "url": "https://picsum.photos/600/400"
+  },
+  "createdBy": "1",
+  "createdDate": 1530238113250
+};
+const fixture_snippet3 = {
+  "id": "3",
+  "type": "text",
+  "description": "Donec lacinia congue felis in faucibus. Donec lacinia congue felis in faucibus ads.",
+  "programId": "1",
+  "createdBy": "1",
+  "createdDate": 1530238113250
+};
+
+const fixture_snippetsSingular = [
+  fixture_snippet2
+];
+const fixture_snippetsMany = [
+  fixture_snippet1,
+  fixture_snippet2,
+  fixture_snippet3,
+];
+
+
+storiesOf('Program Card', module)
+
+  .addDecorator(story => (
+    <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
+  ))
+
+  .add('Type - has program data (light)', () => (
+    <ProgramCard program={fixture_programLightInfo} />
+  ))
+
+  .add('Type - has program data (full)', () => (
+    <ProgramCard program={fixture_programFullInfo} />
+  ))
+
+  .add('Type - light program data + has Snippets (singular)', () => (
+    <ProgramCard program={fixture_programLightInfo} snippets={fixture_snippetsSingular} />
+  ))
+
+  .add('Type - full program data + has Snippets (singular)', () => (
+    <ProgramCard program={fixture_programFullInfo} snippets={fixture_snippetsSingular} />
+  ))
+
+  .add('Type - light program data + has Snippets (many)', () => (
+    <ProgramCard program={fixture_programLightInfo} snippets={fixture_snippetsMany} />
+  ))
+
+;
