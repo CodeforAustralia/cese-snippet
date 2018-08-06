66a7a23eff604f0bc55962b9d446b2186817a0ad
Elise Chant <elise.chant@gmail.com>
2018-08-06 14:13:14 +1000

    snippets

--- src/store/snippets/actionCreators.js
+++ src/store/snippets/actionCreators.js
@@ -8,236 +8,139 @@ import { getFilterKey } from "./helpers";
 
 const log = bows('Snippets');
 
-export const fetchSuccess = (snippets) => {
-  log('fetch success');
+export const fetchByFilterRequest = (filterKey) => {
+  log('fetching by filter');
   return {
-    type: ACTION_TYPES.fetchSuccess,
-    payload: {
-      snippets: objectify(snippets),
-    }
-  }
-};
-
-/**
- * @param data {Array} Snippets to add to the filter
- * @param filterKey {String} key in the format "[school code]_2018_[program id]"
- */
-const createFilter = (data, filterKey = null) => {
-  if (!filterKey) {
-    throw new Error('Must provide filterKey');
-  }
-  return {
-    type: ACTION_TYPES.createFilter,
+    type: ACTION_TYPES.fetchByFilterRequest,
     payload: {
       filterKey,
-      filterValue: data.map(d => d.id), // snippet ids
     }
   }
 };
 
-export const updateFilter = (data, filterKey) => {
+export const fetchByFilterSuccess = (snippets, filterKey) => {
+  log('fetch by filter success');
+  return {
+    type: ACTION_TYPES.fetchByFilterSuccess,
+    payload: {
+      snippets,
+      filterKey,
+    }
+  }
+};
+
+export const fetchByFilterError = (message, filterKey) => {
+  log('fetch by filter error');
+  return {
+    type: ACTION_TYPES.fetchByFilterError,
+    payload: {
+      message,
+      filterKey,
+    }
+  }
+};
+
+export const createRequest = () => {
+  log('creating');
+  return {
+    type: ACTION_TYPES.createRequest,
+  }
+};
+
+export const createSuccess = (snippet) => {
+  log('create success');
+  return {
+    type: ACTION_TYPES.createSuccess,
+    payload: {
+      snippets: objectify(snippet),
+    }
+  }
+};
+
+export const createError = (error) => {
+  log('create error');
+  return {
+    type: ACTION_TYPES.createSuccess,
+    payload: {
+      message: error.message || 'Something went wrong.'
+    }
+  }
+};
+
+export const updateFilter = (ids, filterKey) => {
+  if (!Array.isArray(ids)) {
+    ids = [ids]
+  }
   return {
     type: ACTION_TYPES.updateFilter,
     payload: {
       filterKey,
-      filterValue: data.map(d => d.id),
+      filterValue: ids,
     }
   }
 };
 
-
-export const fetchPageState = (snippets) => {
-  return (dispatch, getState, api) => {
-    return snippets.map(snippet => {
-      const {programId, schoolCode, year} = snippet;
-
-      dispatch({
-        type: ACTION_TYPES.fetchSuccess,
-      })
-
-      dispatch({
-        type: ACTION_TYPES.createFilters,
-      })
-
-    });
-  }
-};
-
-
-
-
-/**
- * Create Snippet Thunk Sequence
- * @param Snippet {Object}
- * @param filterProps {Object} - {schoolCode, year, programId}
- * @returns {Function} Thunk
- */
-export const createSnippet = (snippet, {schoolCode = null, year = null}) => {
-  // Steps:
-  // 1. sanitize input
-  // 2. POST
-  // 3. update byId
-  // 4. *update* a filter to append item
-  //    - do this last in case so filter listeners will get updates
-
-  if (!snippet.programId || !schoolCode || !year) {
-    throw new Error('Must provide "programId", "schoolCode" and "year".');
+export const fetchByFilter = (filterProps) => {
+  if (!filterProps.schoolCode || !filterProps.year || !filterProps.programId) {
+    throw new Error('Must provide filter props to fetch by filter.');
   }
 
-  const filterKey = getFilterKey({
-    schoolCode,
-    year,
-    programId: snippet.programId,
-  });
-
-  log(`Posting (creating): ${JSON.stringify(snippet)}`);
+  const search = queryString.stringify(filterProps);
+  const filterKey = getFilterKey(filterProps);
 
   return (dispatch, getState, api) => {
-
-    dispatch({
-      type: ACTION_TYPES.updateByFilterRequest,
-      payload: {
-        filterKey,
-      }
-    });
-
-    // 2.
-    return api('/snippets', {
-      method: 'POST',
-      body: JSON.stringify(snippet),
-    })
+    dispatch(fetchByFilterRequest(filterKey));
+    return api(`/snippets?${search}`)
       .then((resp) => {
-        if (!resp.data) {
-          throw new Error('Data not provided in response');
+        const snippets = resp.data;
+        if (!snippets) {
+          throw new Error('Data not provided in response.');
         }
-        log(`Posted: ${resp.data}`);
-        // 3.
-        dispatch(fetchSuccess(resp.data));
-
-        return resp;
+        dispatch(fetchByFilterSuccess(snippets, filterKey));
+        return snippets;
       })
-      .then((resp) => {
-        // 4.
-        let d = resp.data;
-
-        if (!Array.isArray(d)) {
-          d = [d]
-        }
-        dispatch(updateFilter(d, filterKey));
-
-        dispatch({
-          type: ACTION_TYPES.updateByFilterSuccess,
-          payload: {
-            filterKey,
-          }
-        });
-
-        return resp;
+      .then((snippets) => {
+        const snippetIds = snippets.map(s => s.id);
+        dispatch(updateFilter(snippetIds, filterKey));
+        return snippets;
       })
       .catch((error) => {
-        log(`Error: ${error}`);
-
-        dispatch({
-          type: ACTION_TYPES.updateByFilterError,
-          payload: {
-            filterKey,
-            message: error,
-          }
-        });
-
-        // todo - status messages
+        dispatch(fetchByFilterError(error));
         return error;
       });
   }
 };
 
-
-/**
- * filterProps {Object} - {schoolCode, year, programId}
- */
-export const fetchSnippetsByProgram = (filterProps) => {
-  const filterKey = getFilterKey(filterProps);
-  const search = queryString.stringify({programId: filterProps.programId, year: filterProps.year});
-  return fetchFromApiOrCache(`/snippets?${search}`, filterKey);
-};
-
-
-
-/**
- * Fetch Programs Thunk Sequence
- * @param path
- * @param filterKey {String}
- * @returns {function(*, *, *)}
- */
-export const fetchFromApiOrCache = (path, filterKey = null) => {
-  // Steps:
-  // 0. Check if item is cached, if it is serve it instead and end.
-  // 1. GET
-  // 2. update byId
-  // 3. *create* new filter values for this search key filter
-  //    - do this last in case so filter listeners will update
-  //    - ONLY DO THIS IF FILTER PROPS ARE PROVIDED - i might fetch program on program detail page not in a filter for example
-
-  log(`Fetching: ${path}`);
-
-  if (!filterKey) {
-    throw new Error('Must provide filterKey.');
+export const createSnippet = (snippet) => {
+  if (!snippet.schoolCode || !snippet.year || !snippet.programId) {
+    throw new Error('Must provide "schoolCode", "year" and "programId" props to Snippet to create.');
   }
 
+  const filterKey = getFilterKey({
+    schoolCode: snippet.schoolCode,
+    year: snippet.year,
+    programId: snippet.programId,
+  });
+
   return (dispatch, getState, api) => {
-
-    // 0.
-    // if (filterKey) {
-    //   const state = getState();
-    //   if (filterKey in state.snippets.filters) {
-    //     return state.snippets.filters[filterKey];
-    //   }
-    // }
-
-    dispatch({
-      type: ACTION_TYPES.fetchByFilterRequest,
-      payload: {
-        filterKey,
-      }
-    });
-
-    // 1.
-    return api(path)
+    dispatch(createRequest());
+    return api('/snippets', {
+      method: 'POST',
+      body: JSON.stringify(snippet),
+    })
       .then((resp) => {
-        if (!resp.data) {
+        const snippet = resp.data;
+        if (!snippet) {
           throw new Error('Data not provided in response');
         }
-        log(`Fetched`);
-        // 2.
-        dispatch(fetchSuccess(resp.data));
-        return resp;
+        dispatch(createSuccess(snippet));
+        return snippet;
       })
-      .then((resp) => {
-        // 3.
-        dispatch(createFilter(resp.data, filterKey));
-
-        dispatch({
-          type: ACTION_TYPES.fetchByFilterSuccess,
-          payload: {
-            filterKey,
-          }
-        });
-
-        return resp;
+      .then((snippet) => {
+        dispatch(updateFilter(snippet, filterKey));
       })
       .catch((error) => {
-        // todo - status messages
-        log(`Error: ${error}`);
-
-        dispatch({
-          type: ACTION_TYPES.fetchByFilterError,
-          payload: {
-            filterKey,
-            message: error,
-          }
-        });
-
-        return error;
-      })
+        dispatch(createError(error));
+      });
   }
 };
